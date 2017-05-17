a670516811d8dfd334f94f5f0c5d2a02cae8ab2b
Vlad Holubiev <golubev.vld@gmail.com>
2017-05-17 19:59:31 +0300

    Upgrade mongo deps in kadira-engine to fix authorization issues

--- kadira-engine/run.sh
+++ kadira-engine/run.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 docker build -t kadira-engine .
-docker run -it -e MONGO_URL=$KADIRA_MONGO_URL -e PORT=11011 -p 11011:11011 kadira-engine
+docker run -it -e MONGO_URL=$KADIRA_MONGO_URL -e MONGO_SHARD_URL_one=$KADIRA_MONGO_URL -e PORT=11011 -p 11011:11011 kadira-engine
