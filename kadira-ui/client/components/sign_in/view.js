7a1db9c57b8431a4074e802b2a5f119d1c8e5d82
Vlad Holubiev <golubev.vld@gmail.com>
2017-05-18 14:54:47 +0300

    kadira-ui: disable meteor login, create admin user on startup

--- kadira-ui/client/components/sign_in/view.js
+++ kadira-ui/client/components/sign_in/view.js
@@ -4,13 +4,5 @@ Template.signIn.events({
     var email = tmpl.$("input[name=email]").val();
     var password = tmpl.$("input[name=password]").val();
     FlowComponents.callAction("signInWithEmail", email, password);
-  },
-  "click #sign-in-with-meteor": function(e) {
-    e.preventDefault();
-    FlowComponents.callAction("signInWithMeteor");
-  },
-  "click #sign-up-with-meteor": function(e) {
-    e.preventDefault();
-    FlowComponents.callAction("signUpWithMeteor");
   }
 });
--- kadira-ui/server/hooks/register.js
+++ kadira-ui/server/hooks/register.js
@@ -1,47 +1,11 @@
-// Sending Email after User Registered
-
-var format = Npm.require("util").format;
-
-// Can only call onCreateUser once
-Accounts.onCreateUser(function(options, user) {
-  if(user) {
-    Meteor.defer(function() {
-      // sending welcome email and onboarding
-      if(Meteor.settings.madmimi) {
-        var username = Meteor.settings.madmimi.username;
-        var apiKey = Meteor.settings.madmimi.apiKey;
-        var userList = Meteor.settings.madmimi.userList;
-
-        var userEmail = encodeURIComponent(AccountsHelpers.getUserEmail(user));
-        var apiFormat = 
-          "http://api.madmimi.com/audience_lists/" + 
-          "%s/add?email=%s&username=%s&api_key=%s";
-        var url = format(apiFormat, userList, userEmail, username, apiKey);
-
-        console.info("sending user to the email list:", userEmail);
-        HTTP.post(url, function(err, response) {
-          // may be do retrying
-          if(err) {
-            var errorMessage = 
-              "error sending user email: " + userEmail +
-              " error: " + err.message;
-            console.error(errorMessage);
-          } else if(response.statusCode !== 200) {
-            var warnMessage = 
-              "error(not 200) sending user email: " + userEmail +
-              " error: " + response.content;
-            console.error(warnMessage);
-          }
-        });
-      }
-
-      // added to metrics
-      UserEvents.track("user", "registered", {
-        userId: user._id,
-        email: AccountsHelpers.getUserEmail(user)
-      });
+Meteor.startup(function() {
+  if (Meteor.users.find().count() < 1) {
+    Accounts.createUser({
+      username: 'admin',
+      email: 'admin@gmail.com',
+      password: 'admin'
     });
-  }
 
-  return user;
+    console.log('CREATED ADMIN USER (admin@gmail.com/admin). CHANGE PASSWORD');
+  }
 });
